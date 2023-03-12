8ad24a7f34c4745d96ff7b3f21c03cfbe2de501c
cojocaru-dan <dan.cojocaru90233@gmail.com>
2023-03-12 11:36:49 -0700

    Methods

--- data.js
+++ data.js
@@ -1,7 +1,51 @@
 // WRITE YOUR CODE BELOW THIS LINE
 
+// 1
+const favouriteBook = {
+    title: "The Namesake",
+    author: "Jhumpa Lahiri",
+    year: 2003,
+    age: function(currentYear) {
+		return currentYear - this.year;
+	},
+    characters: ["Ashoke", "Gogol", "Sonia", "Maxine"]
+};
 
+let ageOfMyFavBook = favouriteBook.age(2023);
+console.log(ageOfMyFavBook);
 
+// 2
+console.log(console);
+
+// 3
+console.log(console.log);
+
+// 4
+for (let i = 0; i < 5; i++) {
+	console.count();
+}
+
+// 5
+console.log(Object.keys(favouriteBook));
+
+// 6
+let aNumber = 47;
+console.log(aNumber.toString(), typeof aNumber.toString());
+
+// 7
+let writers = "George Orwell, Ernest Hemingway, Jane Austen, Agatha Christie, Oscar Wilde";
+const writersArray = writers.split(", ");
+writersArray[0] = "William Shakespeare";
+console.log(writersArray);
+
+// 8
+let writersPlus = writersArray.join("+");
+console.log(writersPlus);
+
+// 9
+writersArray.push("Fyodor Dostoyevsky");
+writersArray.push("Johann Wolfgang Goethe");
+console.log(writersArray);
 // DO NOT MODIFY THE CODE BELOW THIS LINE
 let toExport;
 
